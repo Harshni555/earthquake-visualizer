90322de5de0cb133358d1a6626c47a76dc2ffd46
Harshini <harshiniganngula27@gmail.com>
2025-10-31 16:40:25 +0530

    fix: correct base path for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/earthquake-visualizer",
+  base: "./",
   server: {
-    open: true, // opens the broswer when the server starts
+    open: true, // opens the browser when the server starts
     host: "0.0.0.0", // Allows access from outside the container
-    port: 5173 // Explicitly set the port
-  }
+    port: 5173, // Explicitly set the port
+  },
 });
